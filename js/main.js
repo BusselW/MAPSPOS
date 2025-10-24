b233f388992bfd3177ef23c2a162a8ad7ac40c80
Wes van Bussel <wvbussel91@gmail.com>
2025-10-24 14:30:22 +0200

    Delete js directory

--- js/main.js
+++
@@ -1,5 +0,0 @@
-import App from './ui/App.js';
-
-const h = React.createElement;
-const domContainer = document.querySelector('#react-app');
-ReactDOM.render(h(App), domContainer);
--- js/services/linkService.js
+++
@@ -1,7 +0,0 @@
-export function bouwLink(basisUrl, parameters) {
-    const url = new URL(basisUrl);
-    for (const key in parameters) {
-        url.searchParams.append(key, parameters[key]);
-    }
-    return url.toString();
-}
